284db7b8abbdb2747af220ab3b49c8101c21c8b4
TwilightLemon <cz241126@live.com>
2024-07-18 15:20:21 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AppBarTest;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseBtn_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void AppBar_OnFullScreenStateChanged(object sender, bool e)
+    {
+        Debug.WriteLine("Full Screen State: "+e);
+        Visibility = e ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    private void SelectFlag(object sender, RoutedEventArgs e)
+    {
+        appBar.Location = (sender as Button).Content.ToString() switch {
+            "Top" => AppBarLocation.Top,
+            "Bottom" => AppBarLocation.Bottom,
+            "Left" => AppBarLocation.Left,
+            "Right" => AppBarLocation.Right,
+            "RegisterOnly"=> AppBarLocation.RegisterOnly,
+            _ => AppBarLocation.None
+        };
+    }
+
+    private void OneMore_Click(object sender, RoutedEventArgs e)
+    {
+        new MainWindow() {Title="Window AWA"}.Show();
+    }
+}
